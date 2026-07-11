8ad8fccbecc92765c8cc8a829a8d2e3385195196
thiraviya2024 <thiyathiya088@gmail.com>
2026-07-11 16:06:43 +0530

    Refactor create_vectorstore function and imports
    Updated the import statement for HuggingFaceEmbeddings and added a docstring to the create_vectorstore function.

--- utils/rag.py
+++ utils/rag.py
@@ -1,14 +1,19 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 
 def create_vectorstore(text):
+    """
+    Create a FAISS vector database from the extracted medical report.
+    """
 
+    # Initialize embedding model
     embedding_model = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
 
+    # Split document into chunks
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=200
@@ -16,6 +21,7 @@ def create_vectorstore(text):
 
     docs = splitter.create_documents([text])
 
+    # Create FAISS vector database
     vectorstore = FAISS.from_documents(
         docs,
         embedding_model
